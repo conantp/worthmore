223b79538aed69156588184f4780f6fc3412e892
Patrick Conant <patrick@prcapps.com>
2022-04-21 23:14:55 -0400

    First deploy

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,33 @@
-const path = require('path');
+// const path = require('path');
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, 'src/components/index.js'),
+//       name: 'WorthmoreLib',
+//       fileName: (format) => `worthmore-lib.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ['vue'],
+//       output: {
+//         // Provide global variables to use in the UMD build
+//         // Add external deps here
+//         globals: {
+//           vue: 'Vue',
+//         },
+//       },
+//     },
+//   },
+//   plugins: [vue()]
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.js'),
-      name: 'WorthmoreLib',
-      fileName: (format) => `worthmore-lib.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // Add external deps here
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
   plugins: [vue()]
 })
